e1b8f0031c5a7d6011b8538549d6d37a277ea92c
Dustin Morris <dwmorris.biz@outlook.com>
2017-07-26 15:50:36 -0400

    added responseMsg and handler

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -6,6 +6,15 @@ export default Ember.Controller.extend({
   emailAddress: '',
 
   isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-  isDisabled: Ember.computed.empty('isValid')
+  isDisabled: Ember.computed.empty('isValid'),
+
+  actions: {
+
+    saveInvitation() {
+      alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
+  }
 
 });
